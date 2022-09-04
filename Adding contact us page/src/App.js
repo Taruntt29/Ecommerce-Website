fe91d9cf1214a015919a91f24626ecab32200a3a
Taruntt29 <109497779+Taruntt29@users.noreply.github.com>
2022-09-04 14:45:41 +0530

    Add files via upload

---
+++ Adding contact us page/src/App.js
@@ -0,0 +1,80 @@
+import Header from "./Layout/Header";
+import PageSummary from "./UI/PageSummary";
+import Store from "./Components/Store/Store";
+import Footer from "./Layout/Footer";
+import Cart from "./Components/Cart/Cart";
+import { useState } from "react";
+import CartProvider from "./Components/Context/CartProvider";
+import { Route } from "react-router-dom";
+import About from "./Components/Pages/About";
+import Home from "./Components/Pages/Home";
+import Contact from "./Components/Pages/Contact";
+
+function App() {
+  const [cartClicked, setCartClicked] = useState(false);
+  const cartDisplayHandler = () => {
+    setCartClicked(true);
+  };
+  const cartDisplayHider = () => {
+    setCartClicked(false);
+  };
+  const productsArr = [
+    {
+      title: "Colors",
+      price: 100,
+      imageUrl:
+        "https://prasadyash2411.github.io/ecom-website/img/Album%201.png",
+      key: Math.random().toString(),
+    },
+    {
+      title: "Black and white Colors",
+      price: 50,
+      imageUrl:
+        "https://prasadyash2411.github.io/ecom-website/img/Album%202.png",
+      key: Math.random().toString(),
+    },
+    {
+      title: "Yellow and Black Colors",
+      price: 70,
+      imageUrl:
+        "https://prasadyash2411.github.io/ecom-website/img/Album%203.png",
+      key: Math.random().toString(),
+    },
+    {
+      title: "Blue Color",
+      price: 100,
+      imageUrl:
+        "https://prasadyash2411.github.io/ecom-website/img/Album%204.png",
+      key: Math.random().toString(),
+    },
+  ];
+  return (
+    <>
+      <CartProvider>
+        <Header onClose={cartDisplayHandler}></Header>
+        <Route path='/Home'>
+        {cartClicked && <Cart onClose={cartDisplayHider}></Cart>}
+        <PageSummary></PageSummary>
+         <div> <Home/></div>
+        </Route>
+        <Route path="/Store">
+          <PageSummary></PageSummary>
+          <Store storeItems={productsArr}></Store>
+          {cartClicked && <Cart onClose={cartDisplayHider}></Cart>}
+        </Route>
+        <Route path="/About">
+          <div>
+            {'Helper'}<About></About>
+          </div>
+          {cartClicked && <Cart onClose={cartDisplayHider}></Cart>}
+        </Route>
+        <Route path='/Contact'>
+          <Contact></Contact>
+        </Route>
+
+        <Footer></Footer>
+      </CartProvider>
+    </>
+  );
+}
+export default App;
