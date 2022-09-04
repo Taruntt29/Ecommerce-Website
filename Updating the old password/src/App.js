05145e36188e6fb2fb70ae481afe851a8e214b83
Taruntt29 <109497779+Taruntt29@users.noreply.github.com>
2022-09-04 14:54:28 +0530

    Add files via upload

---
+++ Updating the old password/src/App.js
@@ -0,0 +1,33 @@
+import { useContext, useState } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import CartProvider from './components/Context/CartProvider';
+import CartContext from './components/Context/cart-contetxt';
+import Layout from './components/Layout/Layout';
+import UserProfile from './components/Profile/UserProfile';
+import AuthPage from './pages/AuthPage';
+import HomePage from './pages/HomePage';
+
+function App() {
+  const credCtx = useContext(CartContext);
+  return (
+    <CartProvider>
+    <Layout >
+      <Switch>
+        <Route path='/' exact>
+          <HomePage />
+          <HomePage />  
+        </Route>
+
+        <Route path='/auth'>
+          <AuthPage />
+        </Route>
+
+        <Route path='/profile'>
+          <UserProfile />
+        </Route>
+      </Switch>
+    </Layout>
+    </CartProvider>
+  );
+}
+export default App;
---
+++ Updating the old password/src/components/Layout/MainNavigation.js
@@ -0,0 +1,30 @@
+import { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import CartContext from '../Context/cart-contetxt';
+import CartProvider from '../Context/CartProvider';
+import classes from './MainNavigation.module.css';
+const MainNavigation = (props) => {
+  const credCtx = useContext(CartContext);
+  return (
+    <header className={classes.header}>
+      <Link to='/'>
+        <div className={classes.logo}>React Auth</div>
+      </Link>
+      <nav>
+        <ul>
+          <li>
+            <Link to='/auth'>Login</Link>
+          {!credCtx.check() && <Link to='/auth'>Login</Link>}
+          </li>
+          <li>
+           {credCtx.check() && <Link to='/profile'>Profile</Link>}
+          </li>
+          <li>
+            {credCtx.check() && <button onClick={credCtx.logOut}>Logout</button>}
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+export default MainNavigation;
